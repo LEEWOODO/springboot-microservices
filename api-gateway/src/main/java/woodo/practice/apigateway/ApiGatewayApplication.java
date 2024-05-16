fd65e5fd2b056fbfc089f9adac099d054e8414a6
dnejdzlr2 <dnejdzlr2@virnect.com>
2024-05-16 10:45:34 +0900

    - gateway 추가 - config server 추가 : https://github.com/LEEWOODO/config-server-repo   - configserver properties 파일 변경 되었을 경우 해당 어플리케이션의 actuator/refresh 를 통해 변경된 파일을 reload 하여 가져옴을 확인하였음. 추후 cloud bus 를 도입할 예정

--- api-gateway/src/main/java/woodo/practice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/woodo/practice/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package woodo.practice.configserver;
+package woodo.practice.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
