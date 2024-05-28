3747cc7b4672a5952aa4011021cc1d96a3918a60
dnejdzlr2 <dnejdzlr2@virnect.com>
2024-05-28 15:34:40 +0900

    springdoc-openapi 3버전 swagger 적용 - Controller - DTO - Config

--- department-service/src/main/java/woodo/practice/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/woodo/practice/departmentservice/controller/DepartmentController.java
@@ -1,5 +1,9 @@
 package woodo.practice.departmentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import woodo.practice.departmentservice.dto.DepartmentDto;
 import woodo.practice.departmentservice.service.DepartmentService;
 
+@Tag(name = "Department", description = "Department Rest API")
 @RestController
 @RequestMapping("api/departments")
 @AllArgsConstructor
@@ -14,14 +19,16 @@ public class DepartmentController {
 
     private DepartmentService departmentService;
 
-    // Build save department REST API
+    @Operation(summary = "Save Department", description = "Save Department")
+    @ApiResponse(responseCode = "201", description = "Department saved successfully")
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
         DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
         return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
     }
 
-    // Build get department rest api
+    @Operation(summary = "Get Department", description = "Get Department by department code")
+    @ApiResponse(responseCode = "200", description = "Department found successfully")
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode) {
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
--- employee-service/src/main/java/woodo/practice/employeeservice/dto/EmployeeDto.java
+++ employee-service/src/main/java/woodo/practice/employeeservice/dto/EmployeeDto.java
@@ -1,19 +1,32 @@
 package woodo.practice.employeeservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Employee Data Transfer Object")
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
+    @Schema(description = "Employee ID", example = "1")
     private Long id;
+
+    @Schema(description = "First Name", example = "John")
     private String firstName;
+
+    @Schema(description = "Last Name", example = "Doe")
     private String lastName;
+
+    @Schema(description = "Email", example = "adb@naver.com")
     private String email;
+
+    @Schema(description = "Department Code", example = "D001")
     private String departmentCode;
+
+    @Schema(description = "Organization Code", example = "ABC_ORG")
     private String organizationCode;
 }
